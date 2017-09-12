bc423f93542412ef0045a15657d7588ee13095ee
Oleg Gorbunov <beornborn@gmail.com>
2017-09-12 21:30:27 +0300

    fix activity creation

--- app/sagas/CreateActivity.js
+++ app/sagas/CreateActivity.js
@@ -4,18 +4,16 @@ import { SAGA_CREATE_ACTIVITY } from 'Closies/app/reducers/Saga'
 import * as api from 'Closies/app/api'
 import { perform as fetchActivities } from 'Closies/app/sagas/FetchActivities'
 import { handleResponse } from 'Closies/app/utils/ApiHandlers'
-import { getCurrentUser } from 'Closies/app/reducers/selectors/Data'
 import { NavigationActions } from 'react-navigation'
 import { reset } from 'redux-form'
 
 const prepareParams = function* prepareParams(formData: Object): Generator<*,*,*> {
   const { description, image, longitude, latitude } = formData
-  const user = yield select(getCurrentUser)
 
   let params = {
     latitude,//: 50.4414 + Math.random() * (50.4995 - 50.4014),
     longitude,//: 30.493 + Math.random() * (30.6920 - 30.5030),
-    user_id: user.id,
+    user_in_group_id: 1, // TODO
   }
   if (description && description.value) {
     params = {...params, description: description.value}
