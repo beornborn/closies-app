16ef277b4c1d06a99d3b13dc84781b4dc0dbe9ca
Oleg Gorbunov <beornborn@gmail.com>
2017-10-18 09:13:37 +0300

    upgrade react

--- app/sagas/misc/FetchCurrentLocation.js
+++ app/sagas/misc/FetchCurrentLocation.js
@@ -2,12 +2,21 @@
 import { takeEvery, put } from 'redux-saga/effects'
 import { setCurrentLocation } from 'Closies/app/reducers/App'
 import { SAGA_FETCH_CURRENT_LOCATION } from 'Closies/app/reducers/Saga'
+import { sleep } from 'Closies/app/utils/Common'
 
-const geolocate = () => new Promise(resolve => navigator.geolocation.getCurrentPosition(resolve))
+const geolocate = () => new Promise(resolve => navigator.geolocation.getCurrentPosition(resolve, resolve))
 
 export const perform = function* perform(_a?: Object): Generator<*,*,*> {
-  const location = yield geolocate()
-  yield put(setCurrentLocation(location))
+  while (true) {
+    const location = yield geolocate()
+    console.log(location)
+    if (!location.message) {
+      yield put(setCurrentLocation(location))
+      break
+    } else {
+      yield sleep(1000)
+    }
+  }
 }
 
 const watch = function* watch(): Generator<*,*,*> {
